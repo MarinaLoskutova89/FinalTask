05a728fb7b079ef089d281fa17d6c08716b0dd7d
Marina <mloskutova1589@gmail.com>
2021-07-09 13:16:40 +0400

    Первая версия

---
+++ main.py
@@ -0,0 +1,55 @@
+from pprint import pprint
+import requests
+import json
+
+with open('token_vk_user.txt', 'r', encoding='utf-8') as file_object:
+    token = file_object.read().strip()
+
+class VKUser:
+    url = 'https://api.vk.com/method/'
+
+    def __init__(self, token, version):
+        self.params = {
+            'access_token': token,
+            'v': version
+        }
+
+    def write_json(self, data):
+        with open('response.json', 'w', encoding='utf-8') as file:
+            json.dump(data, file, indent=2, ensure_ascii=False)
+
+
+    def get_photos(self, owner_id, album_id):
+        all_photos_url = self.url + 'photos.get'
+        photos_params = {
+            'owner_id': owner_id,
+            'album_id': album_id,
+            'extended': 1,
+            'photo_sizes': 1
+        }
+        res = requests.get(all_photos_url, params={**self.params, **photos_params}).json()
+        self.write_json(res['response']['items'])
+
+    def get_largest_photo(self, size_dict):
+        if size_dict['width'] >= size_dict['height']:
+            return size_dict['width']
+        else:
+            return size_dict['height']
+
+    def largest_photo(self, file):
+        photos = json.load(open(file))
+        for photo in photos:
+            sizes = photo['sizes']
+            max_photos_url = max(sizes, key=self.get_largest_photo)['url']
+            print(max_photos_url)
+
+    def download_photo(self, url):
+        res = requests.get(url, stream=True)
+        with open('photos.json', 'wb') as file:
+            for chunk in res.inter.content(4096):
+                file.write(chunk)
+
+vk_client = VKUser(token, '5.131')
+vk_client.get_photos('552934290', 'profile')
+vk_client.largest_photo('response.json')
+# vk_client.get_name('response.json')
