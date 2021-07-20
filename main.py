75f0a89ce31279d045635002bc0dc2e21d40376b
Marina <mloskutova1589@gmail.com>
2021-07-20 19:34:47 +0400

    Changes

--- main.py
+++ main.py
@@ -3,21 +3,19 @@ import json
 import os
 import yadisk
 from dataclasses import dataclass
-
-@dataclass
-class VKPic:
-    id: str
-    size: int
-    url: str
-    likes: int
+from pprint import pprint
+# @dataclass
+# class VKPic:
+#     id: str
+#     size: int
+#     url: str
+#     likes: int
 
 with open('token_vk_user.txt', 'r', encoding='utf-8') as file_object:
     token = file_object.read().strip()
 
 with open('token_yandex.txt', 'r', encoding='utf-8') as file_object:
     token_yandex = file_object.read().strip()
-
-#token = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'
 
 class VKUser:
     url = 'https://api.vk.com/method/'
@@ -41,59 +39,82 @@ class VKUser:
             'photo_sizes': 1
         }
         res = requests.get(all_photos_url, params={**self.params, **photos_params}).json()
-        #return res
         self.write_json(res['response']['items'])
         return res
 
-    # def get_largest_photo(self, size_dict):
-    #     if size_dict['width'] >= size_dict['height']:
-    #         return size_dict['width']
-    #     else:
-    #         return size_dict['height']
-    #
+    def get_largest_photo(self, size_dict):
+        if size_dict['width'] >= size_dict['height']:
+            return size_dict['width']
+        else:
+            return size_dict['height']
+
     # def largest_photo(self, file):
     #     url = []
+    #     type = []
     #     photos = json.load(open(file))
     #     for photo in photos:
     #         sizes = photo['sizes']
     #         max_photos_url = max(sizes, key=self.get_largest_photo)['url']
+    #         max_photos_size = max(sizes, key=self.get_largest_photo)['type']
     #         url.append(max_photos_url)
+    #         type.append(max_photos_size)
+    #         max_p = dict(zip(url, type))
     #     return url
-    #
-    # def make_direct(self, direct):
-    #     if not os.path.isdir(direct):
-    #         os.mkdir(direct)
-    #
-    # def download_photo(self, url, filename, direct):
-    #     photo_url = url
-    #     photo_name = filename
-    #     photo = dict(zip(photo_url, photo_name))
-    #     self.make_direct(direct)
-    #     for key, value in photo.items():
-    #         res = requests.get(key, stream=True)
-    #         with open(f'{direct}/{value}.jpg', 'wb') as file:
-    #             for chunk in res.iter_content(4096):
-    #                 file.write(chunk)
+
+    def make_direct(self, direct):
+        if not os.path.isdir(direct):
+            os.mkdir(direct)
+
+    def download_photo(self, url, filename, direct):
+        photo_url = url
+        photo_name = filename
+        photo = dict(zip(photo_url, photo_name))
+        self.make_direct(direct)
+        for key, value in photo.items():
+            res = requests.get(key, stream=True)
+            with open(f'{direct}/{value}.jpg', 'wb') as file:
+                for chunk in res.iter_content(4096):
+                    file.write(chunk)
 
 
 vk_client = VKUser(token, '5.131')
 data = vk_client.get_photos('552934290', 'profile')
-# url = vk_client.largest_photo('response.json')
-# filename = vk_client.get_photos_name('response.json')
-# itema = vk_client.get_photos_name()
-#vk_client.download_photo(url, filename, 'Photo')
-#print(data['response']['items'])
 
 list = []
 for item in data['response']['items']:
-    for size in item['sizes']:
-        ss = size['width'] * size['height']
-        pic = VKPic(item['id'], ss, size['url'], item['likes']['count'])
-        list.append(pic)
-        itema = [d for d in list if d.size == max(d.size for d in list)]
+    list.append({'likes': item['likes']['count'], 'date': item['date'],
+                 'url': item['sizes']})
+sorted_list = sorted(list, key=lambda x: x['likes'])
+p = {}
+for file_name in list:
+    like = file_name['likes']
+    url = max(file_name['url'], key=vk_client.get_largest_photo)['url']
+    size = max(file_name['url'], key=vk_client.get_largest_photo)['type']
+    if file_name['likes'] not in p.keys():
+        name = f'{like}.jpg'
+        p[name] = url
+    else:
+        another_name = str(like) + str(file_name['date'])
+        name = f'{another_name}.jpg'
+        p[name] = url
 
-y = yadisk.YaDisk(token=token_yandex)
-print('Token yandex status:')
-print(y.check_token())
+pprint(p)
 
-y.upload(itema.url, itema.likes + '.jpg')
+
+# print(file_name)
+
+# vk_client.download_photo(url, filename, 'Photo')
+
+
+# list = []
+# for item in data['response']['items']:
+#     for size in item['sizes']:
+#         ss = size['width'] * size['height']
+#         pic = VKPic(item['id'], ss, size['url'], item['likes']['count'])
+#         # pic = [item['id'], ss, size['url'], item['likes']['count']]
+#         list.append(pic)
+#         itema = [d for d in list if d.size == max(d.size for d in list)]
+#     print(itema)
+# y = yadisk.YaDisk(token=token_yandex)
+# y.upload(url, itema.likes + '.jpg')
+
