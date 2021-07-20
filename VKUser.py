943ca17dd380238565d8cb1b217a652e2a308365
Marina <mloskutova1589@gmail.com>
2021-07-21 00:31:23 +0400

    Almoust done

---
+++ VKUser.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+class VKUser:
+    url = 'https://api.vk.com/method/'
+
+    def __init__(self, token, version):
+        self.params = {
+            'access_token': token,
+            'v': version
+        }
+
+    def write_json(self, data):
+        with open('response.json', 'w', encoding='utf-8') as file:
+            json.dump(data, file, indent=2, ensure_ascii=False)
+
+    def get_photos(self, owner_id, album_id):
+        all_photos_url = self.url + 'photos.get'
+        photos_params = {
+            'owner_id': owner_id,
+            'album_id': album_id,
+            'extended': 1,
+            'photo_sizes': 1
+        }
+        res = requests.get(all_photos_url, params={**self.params, **photos_params}).json()
+        self.write_json(res['response']['items'])
+        return res
--- main.py
+++ main.py
@@ -2,14 +2,9 @@ import requests
 import json
 import os
 import yadisk
-from dataclasses import dataclass
-from pprint import pprint
-# @dataclass
-# class VKPic:
-#     id: str
-#     size: int
-#     url: str
-#     likes: int
+import time
+from tqdm import tqdm
+import VKUser
 
 with open('token_vk_user.txt', 'r', encoding='utf-8') as file_object:
     token = file_object.read().strip()
@@ -17,104 +12,48 @@ with open('token_vk_user.txt', 'r', encoding='utf-8') as file_object:
 with open('token_yandex.txt', 'r', encoding='utf-8') as file_object:
     token_yandex = file_object.read().strip()
 
-class VKUser:
-    url = 'https://api.vk.com/method/'
-
-    def __init__(self, token, version):
-        self.params = {
-            'access_token': token,
-            'v': version
-        }
-
-    def write_json(self, data):
-        with open('response.json', 'w', encoding='utf-8') as file:
-            json.dump(data, file, indent=2, ensure_ascii=False)
-
-    def get_photos(self, owner_id, album_id):
-        all_photos_url = self.url + 'photos.get'
-        photos_params = {
-            'owner_id': owner_id,
-            'album_id': album_id,
-            'extended': 1,
-            'photo_sizes': 1
-        }
-        res = requests.get(all_photos_url, params={**self.params, **photos_params}).json()
-        self.write_json(res['response']['items'])
-        return res
-
-    def get_largest_photo(self, size_dict):
-        if size_dict['width'] >= size_dict['height']:
-            return size_dict['width']
-        else:
-            return size_dict['height']
-
-    # def largest_photo(self, file):
-    #     url = []
-    #     type = []
-    #     photos = json.load(open(file))
-    #     for photo in photos:
-    #         sizes = photo['sizes']
-    #         max_photos_url = max(sizes, key=self.get_largest_photo)['url']
-    #         max_photos_size = max(sizes, key=self.get_largest_photo)['type']
-    #         url.append(max_photos_url)
-    #         type.append(max_photos_size)
-    #         max_p = dict(zip(url, type))
-    #     return url
-
-    def make_direct(self, direct):
-        if not os.path.isdir(direct):
-            os.mkdir(direct)
-
-    def download_photo(self, url, filename, direct):
-        photo_url = url
-        photo_name = filename
-        photo = dict(zip(photo_url, photo_name))
-        self.make_direct(direct)
-        for key, value in photo.items():
-            res = requests.get(key, stream=True)
-            with open(f'{direct}/{value}.jpg', 'wb') as file:
-                for chunk in res.iter_content(4096):
-                    file.write(chunk)
-
-
-vk_client = VKUser(token, '5.131')
+vk_client = VKUser.VKUser(token, '5.131')
 data = vk_client.get_photos('552934290', 'profile')
+y = yadisk.YaDisk(token=token_yandex)
+
+def get_largest_photo(size_dict):
+    if size_dict['width'] >= size_dict['height']:
+        return size_dict['width']
+    else:
+        return size_dict['height']
 
 list = []
 for item in data['response']['items']:
     list.append({'likes': item['likes']['count'], 'date': item['date'],
                  'url': item['sizes']})
 sorted_list = sorted(list, key=lambda x: x['likes'])
-p = {}
+photo = {}
+photo_info = []
 for file_name in list:
     like = file_name['likes']
-    url = max(file_name['url'], key=vk_client.get_largest_photo)['url']
-    size = max(file_name['url'], key=vk_client.get_largest_photo)['type']
-    if file_name['likes'] not in p.keys():
-        name = f'{like}.jpg'
-        p[name] = url
+    name = f'{like}.jpg'
+    url = max(file_name['url'], key=get_largest_photo)['url']
+    size = max(file_name['url'], key=get_largest_photo)['type']
+    if name not in photo.keys():
+        photo[name] = url
     else:
-        another_name = str(like) + str(file_name['date'])
+        another_name = str(like) + '_' + str(file_name['date'])
         name = f'{another_name}.jpg'
-        p[name] = url
+        photo[name] = url
+    photo_info.append({'file name': name, 'size': size})
+    with open('photo_info', 'w', encoding='utf-8') as file:
+        json.dump(photo_info, file, indent=2, ensure_ascii=False)
 
-pprint(p)
+direct = '/test/Photo from VK'
+# if not os.path.exists(direct):
+#     y.mkdir(direct)
 
+for key, value in tqdm(photo.items()):
+    time.sleep(0.5)
+    res = requests.get(value, stream=True)
+    data = res.content
+    # with open(f'/Photo from VK/{key}', 'wb') as file:
+    #     for chunk in res.iter_content(4096):
+    #         data = file.write(chunk)
 
-# print(file_name)
-
-# vk_client.download_photo(url, filename, 'Photo')
-
-
-# list = []
-# for item in data['response']['items']:
-#     for size in item['sizes']:
-#         ss = size['width'] * size['height']
-#         pic = VKPic(item['id'], ss, size['url'], item['likes']['count'])
-#         # pic = [item['id'], ss, size['url'], item['likes']['count']]
-#         list.append(pic)
-#         itema = [d for d in list if d.size == max(d.size for d in list)]
-#     print(itema)
-# y = yadisk.YaDisk(token=token_yandex)
-# y.upload(url, itema.likes + '.jpg')
-
+y.upload(direct, data)
